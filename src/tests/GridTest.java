fb406e74ee3960262c567198dee7dfdb2fed8b01
gabrielcespedes <gabriel.cespedes@securitize.io>
2023-03-13 00:44:42 -0300

    Added grid and searh initial test signatures

--- src/tests/GridTest.java
+++ src/tests/GridTest.java
@@ -1,4 +1,35 @@
 package tests;
 
-public class GridTest {
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.CheckoutPage;
+import pages.GridPage;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class GridTest extends BaseTest {
+
+    private LoginPage loginPage;
+    private HomePage homePage;
+
+    private GridPage gridPage;
+
+    @BeforeMethod
+    public void performLoginAndNavigateToGrid() {
+        loginPage = new LoginPage(getWebDriver());
+        loginPage.enterTextUsername(System.getProperty("credential.username"));
+        loginPage.enterTextPassword(System.getProperty("credential.password"));
+        homePage = new HomePage(loginPage.clickButtonSignIn());
+        gridPage = new GridPage(homePage.getTopNavigationPage().clickLinkGrid());
+    }
+
+    @Test
+    public void verifyGridItem() {
+
+    }
+
+    @Test
+    public void verifyAllGridItems() {
+
+    }
 }
--- src/tests/SearchTest.java
+++ src/tests/SearchTest.java
@@ -1,4 +1,34 @@
 package tests;
 
-public class SearchTest {
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+import pages.SearchPage;
+
+public class SearchTest extends BaseTest {
+
+    private LoginPage loginPage;
+    private HomePage homePage;
+    private SearchPage searchPage;
+
+    @BeforeMethod
+    public void performLoginAndNavigateToSearch() {
+        loginPage = new LoginPage(getWebDriver());
+        loginPage.enterTextUsername(System.getProperty("credential.username"));
+        loginPage.enterTextPassword(System.getProperty("credential.password"));
+        homePage = new HomePage(loginPage.clickButtonSignIn());
+        searchPage = new SearchPage(homePage.getTopNavigationPage().clickLinkSearch());
+    }
+
+    @Test
+    public void verifySearchSuccess() {
+
+    }
+
+    @Test
+    public void verifySearchEmpty() {
+
+    }
+
 }
